606ffdc91148ec49f17985e50e11117a650793fa
Hi, I'm Souleymane Walda <souleymanwalda@gmail.com>
2022-11-22 06:53:06 +0800

    kaibo gb
    It consists of almost all the commands of kaibo

---
+++ src/test/java/com/kaibo/KaiboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
